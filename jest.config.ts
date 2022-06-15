d2669efcd935f04afecbb819c426c9a582101ca4
David Marczinus <marczinus.david@gmail.com>
2022-06-15 19:04:42 +0200

    feat: migrate from jest to vitest

--- jest.config.ts
+++
@@ -1,9 +0,0 @@
-export default {
-  clearMocks: true,
-  collectCoverage: false,
-  coverageDirectory: 'coverage',
-  coveragePathIgnorePatterns: ['/node_modules/'],
-  transform: {
-    '^.+\\.(t|j)sx?$': ['@swc/jest'],
-  },
-};
