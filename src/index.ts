563134a026d49be7a25d196759e634e21652d6db
David Marczinus <marczinus.david@gmail.com>
2023-05-17 18:43:00 +0200

    chore: bump deps, fix ts module resolution

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { hello } from './hello/hello';
+import { Bla } from './model/someModule';
+
+const bla = new Bla();
+bla.doIt();
+
+console.log(hello());
---
+++ src/model/someModule.ts
@@ -0,0 +1,5 @@
+export class Bla {
+  public doIt() {
+    console.log('hello');
+  }
+}
